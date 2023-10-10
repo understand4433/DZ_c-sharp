7c311bca6f98ce55e51b11e7e2aba9ba72316cb3
yanepumba <adress>
2023-10-10 14:56:33 +0300

    add First_DZ

---
+++ First_DZ/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу на с#, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number < 100 || number > 999)
+{
+    Console.WriteLine("Это не трехзначное число");
+}
+else
+{
+
+    int secondDigit = (number / 10) % 10;
+    Console.WriteLine("Вторая цифра числа: " + secondDigit);
+}
