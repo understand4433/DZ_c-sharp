a4512b700d9dbd78d3bb487435af1c6c812d438d
yanepumba <adress>
2023-10-10 15:04:58 +0300

    add Second_DZ

---
+++ Second_DZ/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите трехзначное число: ");
+string input = Console.ReadLine();
+
+if (input.Length >= 3)
+{
+    char thirdDigit = input[2];
+    Console.WriteLine($"Третья цифра: {thirdDigit}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет.");
+}
